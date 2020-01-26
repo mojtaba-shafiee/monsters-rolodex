c23f84eacbf96fe577489a8b9b85c89d76dd6e98
Mojtaba-see <59284734+Mojtaba-see@users.noreply.github.com>
2020-01-26 16:25:53 +0330

    deploy
    adding into github

---
+++ precache-manifest.4f65c66542a354f7c40f77ec1e4c78da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b520e29eefc5bdfe4ac2aa41209a38f0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "16e43ee46d46c516b7e6",
+    "url": "/monsters-rolodex/static/css/main.3c881d5e.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16e43ee46d46c516b7e6",
+    "url": "/monsters-rolodex/static/js/main.28d14b87.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
